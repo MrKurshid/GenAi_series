2957541c7ac8cd156bea09cca574d3ba0b67e49a
Kurshid Alam <kurshidalam7715@gmail.com>
2026-06-24 10:16:10 +0530

    basics of memory

---
+++ apps/1_qna_boat.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+import streamlit as st
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+st.title("QnA Boat")
+
+st.markdown("This is a simple QnA app using Google Gemini LLM")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    role = message["role"]
+    content = message["content"]
+    st.chat_message(role).markdown(content)
+
+query = st.chat_input("Ask me anything")
+
+if query:
+    st.session_state.messages.append({"role": "user", "content": query})
+    st.chat_message("user").markdown(query)
+    res = llm.invoke(query)
+    st.session_state.messages.append({"role": "ai", "content": res.content})
+    st.chat_message("ai").markdown(res.content)
